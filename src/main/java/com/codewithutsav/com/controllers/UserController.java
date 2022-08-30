32b488f4540193e6f2dc2882013e2f75e1c6aeb3
Utsav Shergill <utsavshergill@hotmail.com>
2022-08-31 02:14:22 +0530

    Updated Mapping

--- src/main/java/com/codewithutsav/com/controllers/UserController.java
+++ src/main/java/com/codewithutsav/com/controllers/UserController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,6 +29,13 @@ public class UserController {
 		return new ResponseEntity<>(createUserDto ,HttpStatus.CREATED);
 		
 		
+	}
+	
+	@PutMapping("/{userId}")
+	public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userdto, @PathVariable("UserId") Integer uid)
+	{
+		UserDto updatedUser=this.userService.updateUser(userdto,uid);
+		return ResponseEntity.ok(updatedUser);
 	}
 	
 	 
