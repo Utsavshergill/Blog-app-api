8836acea3e68a6acd387664ca667eeba18fd16d8
Utsav Shergill <utsavshergill@hotmail.com>
2022-09-04 02:22:57 +0530

    Validation added for category dto

--- src/main/java/com/codewithutsav/com/payloads/CategoryDto.java
+++ src/main/java/com/codewithutsav/com/payloads/CategoryDto.java
@@ -1,5 +1,8 @@
 package com.codewithutsav.com.payloads;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,7 +13,13 @@ import lombok.Setter;
 public class CategoryDto {
 	
 	private Integer categoryId;
+	
+	@NotEmpty
+	@Size(min=3,message="min size for category title is 3")
 	private String categoryTitle;
+	
+	@NotEmpty
+	@Size(min=15,message="min size of category description is 3")
 	private String categoryDescription;
 
 }
